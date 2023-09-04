7149c1ab87d2b1b9df027246dc198a6d0ee47dd3
drewd03 <andrew.bonner@bsu.edu>
2023-09-04 16:58:21 -0400

    test before refactoring

---
+++ lib/GradeConverter.dart
@@ -0,0 +1,24 @@
+class GradeConverter{
+  late String letter;
+  converter(int grade){
+     if(grade <= 100 && grade >= 93){
+       letter = 'A';
+     }
+     else if(grade <= 92 && grade >= 83){
+       letter = 'B';
+     }
+     else if(grade <= 82 && grade >= 76){
+       letter = 'C';
+     }
+     else if(grade <= 75 && grade >= 66){
+       letter = 'D';
+     }
+     else if(grade <=65 && grade >= 0){
+       letter = 'F';
+     }
+     else{
+       letter = 'No Letter Applicable';
+     }
+
+   }
+}
---
+++ test/gradeconverter_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gradecalc/GradeConverter.dart';
+
+void main(){
+  test('0 is an F',(){
+   var convert = new GradeConverter();
+   convert.converter(0);
+   expect(convert.letter,convert.letter = 'F');
+  });
+  test('65 is an F',(){
+    var convert = new GradeConverter();
+    convert.converter(65);
+    expect(convert.letter,convert.letter = 'F');
+  });
+  test('100 is an A',(){
+    var convert = new GradeConverter();
+    convert.converter(100);
+    expect(convert.letter,convert.letter = 'A');
+  });
+  test('80 is an C',(){
+    var convert = new GradeConverter();
+    convert.converter(80);
+    expect(convert.letter,convert.letter = 'C');
+  });
+}
