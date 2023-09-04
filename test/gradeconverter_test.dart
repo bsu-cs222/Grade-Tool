cd7f992f7f419cb69d9a04bcff4657ab66a662bf
drewd03 <andrew.bonner@bsu.edu>
2023-09-04 17:04:53 -0400

    minor error changes

--- test/gradeconverter_test.dart
+++ test/gradeconverter_test.dart
@@ -3,22 +3,22 @@ import 'package:gradecalc/GradeConverter.dart';
 
 void main(){
   test('0 is an F',(){
-   var convert = new GradeConverter();
+   var convert = GradeConverter();
    convert.converter(0);
    expect(convert.letter,convert.letter = 'F');
   });
   test('65 is an F',(){
-    var convert = new GradeConverter();
+    var convert = GradeConverter();
     convert.converter(65);
     expect(convert.letter,convert.letter = 'F');
   });
   test('100 is an A',(){
-    var convert = new GradeConverter();
+    var convert = GradeConverter();
     convert.converter(100);
     expect(convert.letter,convert.letter = 'A');
   });
   test('80 is an C',(){
-    var convert = new GradeConverter();
+    var convert = GradeConverter();
     convert.converter(80);
     expect(convert.letter,convert.letter = 'C');
   });
