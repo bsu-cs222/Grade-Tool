9099abbb97b7b30e228f72f4600e22c167c6b7c1
drewd03 <andrew.bonner@bsu.edu>
2023-09-04 17:51:43 -0400

    more refactoring to the test code

--- test/gradeconverter_test.dart
+++ test/gradeconverter_test.dart
@@ -2,29 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:gradecalc/GradeConverter.dart';
 
 void main(){
+  List<int> data = [0,65,100,80];
+  List<String> expected = ['F','F','A','C'];
 
-  test('0 is an F',(){
-   var convert = GradeConverter();
-   convert.converter(0);
-   var expected = convert.letter;
-   expect(expected,'F');
-  });
-  test('65 is an F',(){
-    var convert = GradeConverter();
-    convert.converter(65);
-    var expected = convert.letter;
-    expect(expected,'F');
-  });
-  test('100 is an A',(){
-    var convert = GradeConverter();
-    convert.converter(100);
-    var expected = convert.letter;
-    expect(expected,'A');
-  });
-  test('80 is an C',(){
-    var convert = GradeConverter();
-    convert.converter(80);
-    var expected = convert.letter;
-    expect(expected,'C');
-  });
+  for(int i = 0; i < data.length; i++){
+    test('${data[i]} is ${expected[i]}',(){
+      var convert = GradeConverter();
+      convert.converter(data[i]);
+      var result = convert.letter;
+      expect(result,expected[i]);
+    });
+  }
 }
