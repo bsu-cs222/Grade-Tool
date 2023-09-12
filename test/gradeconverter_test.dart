6466e07e1eee0cced84b40f2529396dbf188dfcf
drewd03 <andrew.bonner@bsu.edu>
2023-09-12 01:13:43 -0400

    changed the name of my classes and methods to go along with principles of CC and eliminated some redundancies. also added a UI in the main

--- test/gradeconverter_test.dart
+++ test/gradeconverter_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gradecalc/GradeConverter.dart';
+import 'package:gradecalc/grade_converter.dart';
 
 void main(){
   List<int> data = [0,65,100,80];
@@ -7,8 +7,8 @@ void main(){
 
   for(int i = 0; i < data.length; i++){
     test('${data[i]} is ${expected[i]}',(){
-      var convert = GradeConverter();
-      convert.converter(data[i]);
+      var convert = GradeChange();
+      convert.swap(data[i]);
       var result = convert.letter;
       expect(result,expected[i]);
     });
